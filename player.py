98fe2956494456487f149b6f7f1069c555c75c1b
matth2000 <HermanM@cardiff.ac.uk>
2018-10-18 22:01:18 +0100

    123

---
+++ player.py
@@ -0,0 +1,10 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+mass = items['laptop']['mass'] + items['id']['mass'] + items['money']['mass']
+mass_max = 3
+
+# Start game at the reception
+current_room = rooms["Reception"]
+
